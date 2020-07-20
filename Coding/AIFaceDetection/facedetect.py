e9b52ea43bb6f160f6943c586a2d6fab5fec3eb9
Aaron <aaronisawesome025@gmail.com>
2020-07-20 10:47:54 -0400

    did more

--- Coding/AIFaceDetection/facedetect.py
+++ Coding/AIFaceDetection/facedetect.py
@@ -4,16 +4,29 @@ from random import randrange
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('download.jpg')
+#img = cv2.imread('download.jpg')
 
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Get capture from webcam
+webcam = cv2.VideoCapture(0)
 
+while True:
+    # current frame
+    success, frame = webcam.read()
+
+    # covert to grayscale
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("box", grayscaled_img)
+    cv2.waitKey(1)
+
+"""
+# detect the faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
+# draws the rectangles
 cv2.rectangle(img, face_coordinates,(randrange(256), randrange(256), randrange(256)), 1)
 
-cv2.imshow("box", img)
-
-cv2.waitKey()
+# shows the color image with boxes
+"""
 
 print("Code Completed")
